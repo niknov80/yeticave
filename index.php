e0a99de6d83261e43e2579834fea20e580404d95
niknov <n.novozhilov80@gmail.com>
2019-04-22 15:20:15 +0300

    Добавлен вывод отформатированной стоимости товара в карточке объявления на главной странице

--- index.php
+++ index.php
@@ -45,7 +45,15 @@ $announcements = [
     ],
 ];
 
-// var_dump($announcements[0]['price']);
+
+function get_cost ($cost){
+    $cost = ceil($cost);
+
+    $cost = number_format($cost, 0, '.', ' ');
+    $cost = $cost . " ₽";
+    return $cost;
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -133,7 +141,7 @@ $announcements = [
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?=$item['price']; ?><b class="rub">р</b></span>
+                                <span class="lot__cost"><?=get_cost($item['price']); ?></span>
                             </div>
                             <div class="lot__timer timer">
                             </div>
