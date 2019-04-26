8d8f850d9951c38570ee4cadc2b26764539e2634
niknov80 <n.novozhilov80@gmail.com>
2019-04-26 20:10:11 +0300

    Созданы add на ноуте

--- add.php
+++ add.php
@@ -2,8 +2,10 @@
 require_once('function.php');
 require_once('data.php');
 
-$page_content = include_template('templates/add.php', $timer);
- 
+
+var_dump($_POST);
+
+$page_content = include_template('templates/add.php', ['category' => $category]); 
 $page_layout = include_template('templates/layout.php', ['category' => $category, 
                                           'pageTitle' => $page_title, 
                                           'pageContent' => $page_content, 
--- templates/add.php
+++ templates/add.php
@@ -1,27 +1,14 @@
 <main>
   <nav class="nav">
     <ul class="nav__list container">
-      <li class="nav__item">
-        <a href="all-lots.html">Доски и лыжи</a>
-      </li>
-      <li class="nav__item">
-        <a href="all-lots.html">Крепления</a>
-      </li>
-      <li class="nav__item">
-        <a href="all-lots.html">Ботинки</a>
-      </li>
-      <li class="nav__item">
-        <a href="all-lots.html">Одежда</a>
-      </li>
-      <li class="nav__item">
-        <a href="all-lots.html">Инструменты</a>
-      </li>
-      <li class="nav__item">
-        <a href="all-lots.html">Разное</a>
-      </li>
+      <?php foreach ($template_data['category'] as  $value): ?>                
+            <li class="nav__item">
+                <a href="all-lots.html"><?=htmlspecialchars($value); ?></a>
+            </li>
+      <?php endforeach; ?>      
     </ul>
   </nav>
-  <form class="form form--add-lot container form--invalid" action="https://echo.htmlacademy.ru" method="post"> <!-- form--invalid -->
+  <form class="form form--add-lot container form--invalid" action="add.php" method="post"> <!-- form--invalid -->
     <h2>Добавление лота</h2>
     <div class="form__container-two">
       <div class="form__item form__item--invalid"> <!-- form__item--invalid -->
